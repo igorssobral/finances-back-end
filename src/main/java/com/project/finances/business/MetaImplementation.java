ca4eb2532b08b48e9781d0db9611eb93a7123305
Igor Sobral <igorgmz1421@gmail.com>
2024-01-19 22:39:44 -0300

    add: Meta interface implementation

---
+++ src/main/java/com/project/finances/business/MetaImplementation.java
@@ -0,0 +1,43 @@
+package com.project.finances.business;
+
+import com.project.finances.business.interfaces.MetaService;
+import com.project.finances.model.entity.Meta;
+
+import java.util.List;
+
+public class MetaImplementation implements MetaService {
+    @Override
+    public Meta save(Meta meta) {
+        return null;
+    }
+
+    @Override
+    public Meta update(Meta meta) {
+        return null;
+    }
+
+    @Override
+    public List<Meta> find(Meta meta) {
+        return null;
+    }
+
+    @Override
+    public List<Meta> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Meta> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public Meta findById(Long id) {
+        return null;
+    }
+}
