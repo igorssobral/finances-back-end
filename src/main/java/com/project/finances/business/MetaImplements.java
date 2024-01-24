156f09d3305fede7ddba6112fe79ccdffcc82834
Igor Sobral <igorgmz1421@gmail.com>
2024-01-23 21:14:51 -0300

    add: methods of the interface meta

--- src/main/java/com/project/finances/business/MetaImplements.java
+++ src/main/java/com/project/finances/business/MetaImplements.java
@@ -2,42 +2,99 @@ package com.project.finances.business;
 
 import com.project.finances.business.interfaces.MetaService;
 import com.project.finances.model.entity.Meta;
+import com.project.finances.model.repository.MetaDAORepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class MetaImplements implements MetaService {
+
+    @Autowired
+    private MetaDAORepository metaRepository;
+
     @Override
     public Meta save(Meta meta) {
-        return null;
+        if (meta.getId() != null) {
+            throw new IllegalStateException("The user already exists in the database.");
+        }
+
+        return metaRepository.save(meta);
     }
 
     @Override
     public Meta update(Meta meta) {
-        return null;
+        if (meta.getId() == null) {
+            throw new IllegalStateException("Id cannot be null");
+        }
+        return metaRepository.save(meta);
     }
 
     @Override
-    public List<Meta> find(Meta meta) {
-        return null;
+    public List<Meta> find(Meta filter) {
+
+        Example<Meta> filters = Example.of(filter,
+                ExampleMatcher.matching()
+                        .withIgnoreCase()
+                        .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));
+
+
+        return metaRepository.findAll(filters);
     }
 
     @Override
     public List<Meta> findAll() {
-        return null;
+        return metaRepository.findAll();
     }
 
     @Override
-    public List<Meta> findByName(String name) {
-        return null;
+    public Meta findByName(String name) {
+
+        if (name == null) {
+            throw new IllegalStateException("Name cannot be null!");
+        }
+
+        Meta metaFind = null;
+
+        List<Meta> allMetas = metaRepository.findAll();
+        for (int i = 0; i < allMetas.size(); i++) {
+            if (allMetas.get(i).getName().equals(name)) {
+                metaFind = allMetas.get(i);
+            }
+        }
+
+        return metaFind;
     }
 
     @Override
     public void delete(Long id) {
 
+        if (id == null) {
+            throw new IllegalStateException(String.format("Could not find a entity with id=%l", id));
+        }
+
+        Meta meta = findById(id);
+
+        if (meta == null) {
+            throw new IllegalStateException(String.format("Could not find a entity with id=%l", id));
+        }
+        metaRepository.delete(meta);
     }
 
     @Override
     public Meta findById(Long id) {
-        return null;
+
+        if (id == null) {
+            throw new IllegalStateException("Id cannot be null!");
+        }
+
+        Optional<Meta> optional = metaRepository.findById(id);
+
+
+        return optional.isPresent() ? optional.get() : null;
     }
 }
