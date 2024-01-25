98507b9828dc86dbd53fa7bda831b5142d0daf66
Igor Sobral <igorgmz1421@gmail.com>
2024-01-25 20:28:55 -0300

    add: TransactionDTO

---
+++ src/main/java/com/project/finances/presentation/dto/TransactionDTO.java
@@ -0,0 +1,45 @@
+package com.project.finances.presentation.dto;
+
+import jakarta.persistence.Column;
+
+import java.util.Date;
+
+public class TransactionDTO {
+
+    private Long id;
+    private String name;
+    private Double value;
+    private Date date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
